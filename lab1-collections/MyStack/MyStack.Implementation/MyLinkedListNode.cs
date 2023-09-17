0e06089c55238579d9b347599782f2ffeb10a96a
SashaBondarchuk <o.bondarchuk9962@gmail.com>
2023-09-17 15:53:46 +0300

    added implementation of linked list node

---
+++ lab1-collections/MyStack/MyStack.Implementation/MyLinkedListNode.cs
@@ -0,0 +1,14 @@
+﻿namespace MyStack.Implementation
+{
+    public sealed class MyLinkedListNode<T>
+    {
+        public T Data { get; internal set; }
+        public MyLinkedListNode(T data)
+        {
+            Data = data;
+        }
+
+        public MyLinkedListNode<T>? Next { get; internal set; }
+        public MyLinkedListNode<T>? Prev { get; internal set; }
+    }
+}
