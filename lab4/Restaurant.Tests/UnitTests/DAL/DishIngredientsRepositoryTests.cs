9d3b03cb0b1ea32092bc5c5c4997f440609aa437
SashaBondarchuk <o.bondarchuk9962@gmail.com>
2023-12-12 22:13:01 +0200

    added DAL tests

---
+++ lab4/Restaurant.Tests/UnitTests/DAL/DishIngredientsRepositoryTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant.DAL.Context;
+using Restaurant.DAL.Entities;
+using Restaurant.DAL.Repository;
+
+namespace Restaurant.Tests.UnitTests.DAL
+{
+    [TestFixture]
+    public class DishIngredientsRepositoryTests
+    {
+        private DbContextOptions<RestaurantDbContext> _options;
+
+        [SetUp]
+        public void Setup()
+        {
+            _options = new DbContextOptionsBuilder<RestaurantDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+        }
+
+        [Test]
+        public async Task GetDishIngredientsByDishIdAsync_ReturnsDishIngredientsForDish()
+        {
+            using var context = new RestaurantDbContext(_options);
+            var repository = new DishIngredientsRepository(context);
+
+            var dishId = 10;
+            var sampleDishIngredient = new DishIngredient { DishId = dishId, IngredientId = 1 };
+            await repository.AddAsync(sampleDishIngredient);
+            await repository.SaveAsync();
+
+            var result = await repository.GetDishIngredientsByDishIdAsync(dishId);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Count(), Is.GreaterThan(0));
+                Assert.That(result.Any(di => di.DishId == dishId), Is.True);
+            });
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            using var context = new RestaurantDbContext(_options);
+            context.Database.EnsureDeleted();
+        }
+    }
+}
---
+++ lab4/Restaurant.Tests/UnitTests/DAL/DishesRepositoryTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant.DAL.Context;
+using Restaurant.DAL.Entities;
+using Restaurant.DAL.Repository;
+
+namespace Restaurant.Tests.UnitTests.DAL
+{
+    [TestFixture]
+    public class DishesRepositoryTests
+    {
+        private DbContextOptions<RestaurantDbContext> _options;
+
+        [SetUp]
+        public void Setup()
+        {
+            _options = new DbContextOptionsBuilder<RestaurantDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+        }
+
+        [Test]
+        public async Task GetDishesWithAllInfoAsync_ReturnsDishesWithRelatedInfo()
+        {
+            using var context = new RestaurantDbContext(_options);
+            var repository = new DishesRepository(context);
+
+            var sampleDish = new Dish("Sample Dish", "Sample Description");
+            var sampleIngredient = new Ingredient("Sample Ingredient", "Sample Ingredient Description");
+            
+            sampleDish.Ingredients.Add(sampleIngredient);
+            
+            await repository.AddAsync(sampleDish);
+            await repository.SaveAsync();
+
+            var result = await repository.GetDishesWithAllInfoAsync();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Count(), Is.GreaterThan(0));
+                Assert.That(result.Any(d => d.Ingredients.Any()), Is.True);
+            });
+        }
+
+        [Test]
+        public async Task GetDishWithAllInfoByIdAsync_ReturnsDishWithRelatedInfo()
+        {
+            using var context = new RestaurantDbContext(_options);
+            var repository = new DishesRepository(context);
+
+            var sampleDish = new Dish("Sample Dish", "Sample Description");
+            var sampleIngredient = new Ingredient("Sample Ingredient", "Sample Ingredient Description");
+            
+            sampleDish.Ingredients.Add(sampleIngredient);
+            
+            await repository.AddAsync(sampleDish);
+            await repository.SaveAsync();
+
+            var result = await repository.GetDishWithAllInfoByIdAsync(sampleDish.Id);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Ingredients, Is.Not.Null);
+            Assert.That(result.Ingredients, Is.Not.Empty);
+        }
+    }
+}
